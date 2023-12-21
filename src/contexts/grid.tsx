e2a92901f5ad1d724fe19a3a7b91f3002d62c12d
Nicolas Froidure <nicolas.froidure@gmail.com>
2023-12-21 23:06:58 +0100

    chore(dev): add rem measurement hook

--- src/contexts/grid.tsx
+++ src/contexts/grid.tsx
@@ -1,28 +1,54 @@
+"use client";
+
 import { type ReactNode, createContext } from "react";
 import useCSSVar from "../hooks/useCSSVar";
 import styles from "./grid.module.scss";
 
-export const DEFAULT_GRID_H = parseFloat(styles.hGrid.replace("rem", "")) * 16;
-export const DEFAULT_GRID_V = parseFloat(styles.vGrid.replace("rem", "")) * 16;
+const DEFAULT_GRID_H = parseFloat(styles.hGrid.replace("rem", ""));
+const DEFAULT_GRID_V = parseFloat(styles.vGrid.replace("rem", ""));
+const DEFAULT_V_RHYTHM_RATIO = parseFloat(styles.vRhythmRatio);
+const DEFAULT_COLUMN_RATIO = parseFloat(styles.columnRatio);
+const DEFAULT_GUTTER_RATIO = parseFloat(styles.gutterRatio);
 
 export const GridContext = createContext({
-  h: DEFAULT_GRID_H,
-  v: DEFAULT_GRID_V,
+  vGrid: DEFAULT_GRID_H,
+  hGrid: DEFAULT_GRID_V,
+  vRhythmRatio: DEFAULT_V_RHYTHM_RATIO,
+  columnRatio: DEFAULT_COLUMN_RATIO,
+  gutterRatio: DEFAULT_GUTTER_RATIO,
 });
 
-export function ProvideGridContext({
+export default function ProvideGridContext({
   children,
 }: {
   children: ReactNode;
 }): ReactNode {
   const vGrid = useCSSVar("number", "--vGrid", DEFAULT_GRID_V);
   const hGrid = useCSSVar("number", "--hGrid", DEFAULT_GRID_H);
+  const vRhythmRatio = useCSSVar(
+    "number",
+    "--vRhythmRatio",
+    DEFAULT_V_RHYTHM_RATIO
+  );
+  const gutterRatio = useCSSVar(
+    "number",
+    "--gutterRatio",
+    DEFAULT_COLUMN_RATIO
+  );
+  const columnRatio = useCSSVar(
+    "number",
+    "--columnRatio",
+    DEFAULT_GUTTER_RATIO
+  );
 
   return (
     <GridContext.Provider
       value={{
-        v: vGrid,
-        h: hGrid,
+        vGrid,
+        hGrid,
+        vRhythmRatio,
+        gutterRatio,
+        columnRatio,
       }}
     >
       {children}
