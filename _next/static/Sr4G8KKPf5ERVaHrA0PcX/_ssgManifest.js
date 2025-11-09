6df8be441bf625441d5ae2e908e1e31af5cc5483
nfroidure <nicolas.froidure@gmail.com>
2025-11-09 18:20:57 +0000

    Deploying to gh-pages from @ nfroidure/politics@12f00950f31f9c77ca3405c34b4eee824feb9af5 🚀

---
+++ _next/static/Sr4G8KKPf5ERVaHrA0PcX/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[...slug]","\u002Fagenda\u002F[id]","\u002Fagenda\u002Fpages\u002F[page]","\u002Fblog\u002F[id]","\u002Fblog\u002Fpages\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
